e4202bf5a7c3d3c98b042fbab6a4101d1fe4cde1
Fishrock123 <fishrock123@rocketmail.com>
2014-03-26 16:21:47 -0400

    The tests fail. Dunno why.
    The server gets `{ data: "cats" }` before` { latency: <n> }`.

---
+++ index.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  server: function (primus, options) {
+    primus.Spark.writable('latency', Infinity)
+
+    primus.transform('incoming', function (packet) {
+      if (packet.data.latency) {
+        this.latency = packet.data.latency
+        return false
+      } else {
+        return true
+      }
+    })
+
+  },
+
+  client: function (primus, options) {
+    primus.on('incoming::pong', function () {
+      primus.write({ latency: primus.latency })
+    })
+
+    primus.on('incoming::open', function () {
+      primus.write({ latency: primus.latency })
+    })
+  }
+}
---
+++ test.js
@@ -0,0 +1,56 @@
+var Primus = require('primus')
+  , latency = require('./')
+  , http = require('http').Server
+  , expect = require('expect.js')
+  , opts = { transformer: 'websockets' }
+  , primus, srv
+
+// creates the client
+function client(srv, primus, port) {
+  var addr = srv.address()
+  var url = 'http://' + addr.address + ':' + (port || addr.port)
+  return new primus.Socket(url)
+}
+
+// creates the server
+function server(srv, opts) {
+  return Primus(srv, opts).use('spark-latency', latency)
+}
+
+describe('primus-spark-latency', function () {
+
+  beforeEach(function beforeEach(done) {
+    srv = http()
+    primus = server(srv, opts)
+    done()
+  })
+
+  afterEach(function afterEach(done) {
+    primus.end()
+    done()
+  })
+
+  it('Spark.latency should be a number', function (done) {
+    srv.listen(function () {
+      primus.on('connection', function (spark) {
+        expect(spark.latency).to.be.a('number')
+        done()
+      })
+      client(srv, primus)
+    })
+  })
+
+  it('Spark.latency should be set on connection', function (done) {
+    srv.listen(function () {
+      primus.on('connection', function (spark) {
+        spark.on('data', function (data) {
+          expect(spark.latency).to.be.below(30)
+          done()
+        })
+      })
+      setTimeout( function() {
+        client(srv, primus).write({ data: "cats" })
+      }, 200)
+    })
+  })
+})
