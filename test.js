56d9aa8b138642369a301f69d1f9ff1f154a7aac
Fishrock123 <fishrock123@rocketmail.com>
2014-03-28 16:14:17 -0400

    Add tests for 56e6ea5 + Slight tests refactor.

--- test.js
+++ test.js
@@ -3,13 +3,14 @@ var Primus = require('primus')
   , http = require('http').Server
   , expect = require('expect.js')
   , opts = { transformer: 'websockets' }
+  , alt_opts = { transformer: 'websockets', use_clock_offset: true }
   , primus, srv
 
 // creates the client
-function client(srv, primus, port) {
+function client(srv, primus, opts, port) {
   var addr = srv.address()
   var url = 'http://' + addr.address + ':' + (port || addr.port)
-  return new primus.Socket(url)
+  return new primus.Socket(url, opts)
 }
 
 // creates the server
@@ -21,7 +22,6 @@ describe('primus-spark-latency', function () {
 
   beforeEach(function beforeEach(done) {
     srv = http()
-    primus = server(srv, opts)
     done()
   })
 
@@ -30,48 +30,108 @@ describe('primus-spark-latency', function () {
     done()
   })
 
-  it('Spark.latency should be a number', function (done) {
-    srv.listen(function () {
-      primus.on('connection', function (spark) {
-        expect(spark.latency).to.be.a('number')
-        done()
-      })
-      client(srv, primus)
-    })
-  })
+  describe('Spark.latency', function () {
 
-  it('Spark.latency should be set on connection', function (done) {
-    srv.listen(function () {
-      primus.on('connection', function (spark) {
-        spark.on('data', function (data) {
-          expect(spark.latency).to.be.within(1, 30)
-          done()
-        })
-      })
-      client(srv, primus).on('open', function () {
-        var that = this
-        process.nextTick(function () {
-          that.write({ data: "cats" })
-        })
-      })
-    })
-  })
-
-  it('Ensuring packets cannot set Spark.latency as non-numeric', function (done) {
-    srv.listen(function () {
-      primus.on('connection', function (spark) {
-        spark.on('data', function (data) {
+    it('Should be a number', function (done) {
+      primus = server(srv, opts)
+      srv.listen(function () {
+        primus.on('connection', function (spark) {
           expect(spark.latency).to.be.a('number')
-          expect(spark.latency).to.be.within(1, 30)
           done()
         })
+        client(srv, primus, opts)
       })
-      client(srv, primus).on('open', function () {
-        var that = this
-        process.nextTick(function () {
-          that.write({ _latency: "doge" })
+    })
+
+    it('Should be set on connection', function (done) {
+      primus = server(srv, opts)
+      srv.listen(function () {
+        primus.on('connection', function (spark) {
+          spark.on('data', function (data) {
+            expect(spark.latency).to.be.within(1, 30)
+            done()
+          })
+        })
+        client(srv, primus, opts).on('open', function () {
+          var that = this
+          process.nextTick(function () {
+            that.write({ data: "cats" })
+          })
+        })
+      })
+    })
+
+    it('Should be unable to be set by packets as non-numeric', function (done) {
+      primus = server(srv, opts)
+      srv.listen(function () {
+        primus.on('connection', function (spark) {
+          spark.on('data', function (data) {
+            expect(spark.latency).to.be.a('number')
+            expect(spark.latency).to.be.within(1, 30)
+            done()
+          })
+        })
+        client(srv, primus, opts).on('open', function () {
+          var that = this
+          process.nextTick(function () {
+            that.write({ _latency: "doge" })
+          })
         })
       })
     })
   })
+
+  describe('Spark.clock_offset', function () {
+
+    it('Should be a number', function (done) {
+      primus = server(srv, alt_opts)
+      srv.listen(function () {
+        primus.on('connection', function (spark) {
+          expect(spark.clock_offset).to.be.a('number')
+          done()
+        })
+        client(srv, primus, alt_opts)
+      })
+    })
+
+    it('Should be set on connection', function (done) {
+      primus = server(srv, alt_opts)
+      srv.listen(function () {
+        primus.on('connection', function (spark) {
+          spark.on('data', function (data) {
+            expect(spark.clock_offset).to.be.within(1, 50)
+            done()
+          })
+        })
+        client(srv, primus, alt_opts).on('open', function () {
+          var that = this
+          process.nextTick(function () {
+            that.write({ _latency: that.latency, _clock: Date.now() - 15 })
+            process.nextTick(function() {
+              that.write({ data: "cats" })
+            })
+          })
+        })
+      })
+    })
+
+    it('Should be unable to be set by packets as non-numeric', function (done) {
+      primus = server(srv, alt_opts)
+      srv.listen(function () {
+        primus.on('connection', function (spark) {
+          spark.on('data', function (data) {
+            expect(spark.clock_offset).to.be.a('number')
+            done()
+          })
+        })
+        client(srv, primus, alt_opts).on('open', function () {
+          var that = this
+          process.nextTick(function () {
+            that.write({ _clock: "doge" })
+          })
+        })
+      })
+    })
+  })
+
 })
